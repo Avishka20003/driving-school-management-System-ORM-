86b251e3f46809f0f08b1cc8b033aece852b02c1
Avishka <dilshandell333@gmail.com>
2025-09-25 17:07:38 +0530

    creat Payment mapper class

---
+++ src/main/java/lk/ijse/mapper/PaymentMapper.java
@@ -0,0 +1,47 @@
+package lk.ijse.mapper;
+
+
+import lk.ijse.dto.PaymentsDTO;
+import lk.ijse.dto.tm.PaymentsTM;
+import lk.ijse.entity.Payment;
+
+public class PaymentMapper {
+
+    public static PaymentsDTO toDTO(Payment payment) {
+        if (payment == null) return null;
+
+        PaymentsDTO dto = new PaymentsDTO();
+        dto.setPaymentId(payment.getPaymentId());
+        dto.setPaymentDate(payment.getPaymentDate());
+        dto.setAmount(payment.getAmount());
+        dto.setPaymentMethod(payment.getPaymentMethod());
+        dto.setStatus(payment.getStatus());
+        dto.setStudentId(payment.getStudent() != null ? payment.getStudent().getStudentId() : null);
+
+        return dto;
+    }
+
+    public static Payment toEntity(PaymentsDTO dto) {
+        if (dto == null) return null;
+
+        Payment payment = new Payment();
+        payment.setPaymentId(dto.getPaymentId());
+        payment.setPaymentDate(dto.getPaymentDate());
+        payment.setAmount(dto.getAmount());
+        payment.setPaymentMethod(dto.getPaymentMethod());
+        payment.setStatus(dto.getStatus());
+        return payment;
+    }
+
+    public static PaymentsTM toTM(PaymentsTM dto){
+        if (dto == null) return null;
+
+        PaymentsTM payment = new PaymentsTM();
+        payment.setPaymentId(dto.getPaymentId());
+        payment.setPaymentDate(dto.getPaymentDate());
+        payment.setAmount(dto.getAmount());
+        payment.setPaymentMethod(dto.getPaymentMethod());
+        payment.setStatus(dto.getStatus());
+        return payment;
+    }
+}
