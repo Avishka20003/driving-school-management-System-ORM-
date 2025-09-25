f7db2eb614c9b1953595b39b76e9b4baad4f409d
Avishka <dilshandell333@gmail.com>
2025-09-25 18:53:06 +0530

    creat LessonsBo

---
+++ src/main/java/lk/ijse/bo/custom/LessonsBO.java
@@ -0,0 +1,24 @@
+package lk.ijse.bo.custom;
+
+
+import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.LessonsDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LessonsBO extends SuperBO {
+    List<LessonsDTO> getAllLessons() throws Exception;
+
+    String getLastLessonId() throws Exception;
+
+    boolean saveLessons(LessonsDTO t) throws Exception;
+
+    boolean updateLessons(LessonsDTO t) throws Exception;
+
+    boolean deleteLessons(String id) throws Exception;
+
+    List<String> getAllLessonIds() throws Exception;
+
+    Optional<LessonsDTO> findByLessonId(String id) throws Exception;
+}
