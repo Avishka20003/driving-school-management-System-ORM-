ae00d937ccba1e0cf1d894b6b90acf50ea0af774
Avishka <dilshandell333@gmail.com>
2025-09-25 18:54:07 +0530

    creat InstructorBOBo

---
+++ src/main/java/lk/ijse/bo/custom/InstructorBO.java
@@ -0,0 +1,27 @@
+package lk.ijse.bo.custom;
+
+import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.InstructorDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InstructorBO extends SuperBO {
+    List<InstructorDTO> getAllInstructors() throws Exception;
+
+    String getLastInstructorId() throws Exception;
+
+    boolean saveInstructors(InstructorDTO t) throws Exception;
+
+    boolean updateInstructors(InstructorDTO t) throws Exception;
+
+    boolean deleteInstructors(String id) throws Exception;
+
+    List<String> getAllInstructorIds() throws Exception;
+
+    Optional<InstructorDTO> findByInstructorId(String id) throws Exception;
+
+    String generateNewInstructorsId();
+
+
+}
