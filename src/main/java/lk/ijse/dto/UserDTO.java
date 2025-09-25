380225b64dc90e15966376fd54b1df488dd221e9
Avishka <dilshandell333@gmail.com>
2025-09-25 16:45:33 +0530

    creat dto classes

---
+++ src/main/java/lk/ijse/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package lk.ijse.dto;
+
+import lk.ijse.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDTO {
+    private String userId;
+    private String username;
+    private String password;
+    private Role role;
+    private String email;
+    private String status;
+}
