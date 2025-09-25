6461157ecda0ea613cfa2e9df74c8c7050511a72
Avishka <dilshandell333@gmail.com>
2025-09-25 17:08:26 +0530

    creat StudentCourseDetails mapper class

---
+++ src/main/java/lk/ijse/mapper/StudentCourseDetailsMapper.java
@@ -0,0 +1,33 @@
+package lk.ijse.mapper;
+
+
+import lk.ijse.dto.StudentCourseDetailsDTO;
+import lk.ijse.entity.StudentCourseDetails;
+
+public class StudentCourseDetailsMapper {
+
+    public static StudentCourseDetailsDTO toDTO(StudentCourseDetails scd) {
+        if (scd == null) return null;
+
+        StudentCourseDetailsDTO dto = new StudentCourseDetailsDTO();
+        dto.setStudentCourseId(scd.getStudentCourseId());
+        dto.setStudentId(scd.getStudent() != null ? scd.getStudent().getStudentId() : null);
+        dto.setCourseId(scd.getCourse() != null ? scd.getCourse().getCourseId() : null);
+        dto.setEnrollmentDate(scd.getEnrollmentDate());
+        dto.setStatus(scd.getStatus());
+        dto.setGrade(scd.getGrade());
+
+        return dto;
+    }
+
+    public static StudentCourseDetails toEntity(StudentCourseDetailsDTO dto) {
+        if (dto == null) return null;
+
+        StudentCourseDetails scd = new StudentCourseDetails();
+        scd.setStudentCourseId(dto.getStudentCourseId());
+        scd.setEnrollmentDate(dto.getEnrollmentDate());
+        scd.setStatus(dto.getStatus());
+        scd.setGrade(dto.getGrade());
+        return scd;
+    }
+}
