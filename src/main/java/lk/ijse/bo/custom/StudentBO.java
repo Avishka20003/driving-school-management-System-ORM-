f7120953924ae8c12c853ce04ac747242043167e
Avishka <dilshandell333@gmail.com>
2025-09-25 18:50:21 +0530

    creat QueryBo

--- src/main/java/lk/ijse/bo/custom/StudentBO.java
+++ src/main/java/lk/ijse/bo/custom/StudentBO.java
@@ -1,4 +1,27 @@
 package lk.ijse.bo.custom;
 
-public interface StudentBO {
+
+
+import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.StudentDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentBO extends SuperBO {
+    List<StudentDTO> getAllStudents() throws Exception;
+
+    String getLastStudentId() throws Exception;
+
+    boolean saveStudents(StudentDTO t) throws Exception;
+
+    boolean updateStudents(StudentDTO t) throws Exception;
+
+    boolean deleteStudents(String id) throws Exception;
+
+    List<String> getAllStudentIds() throws Exception;
+
+    Optional<StudentDTO> findByStudentId(String id) throws Exception;
+
+    String generateNewStudentId();
 }
