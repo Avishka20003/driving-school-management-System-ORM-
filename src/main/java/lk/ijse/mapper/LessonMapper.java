61167c60810162b912cb8c796846a87c2cbeb6cd
Avishka <dilshandell333@gmail.com>
2025-09-25 17:06:54 +0530

    creat Lesson mapper class

---
+++ src/main/java/lk/ijse/mapper/LessonMapper.java
@@ -0,0 +1,48 @@
+package lk.ijse.mapper;
+
+import lk.ijse.dto.LessonsDTO;
+import lk.ijse.dto.tm.LessonsTM;
+import lk.ijse.entity.Lesson;
+
+public class LessonMapper {
+
+    public static LessonsDTO toDTO(Lesson
+                                           lesson) {
+        if (lesson == null) return null;
+
+        LessonsDTO dto = new LessonsDTO();
+        dto.setLessonId(lesson.getLessonId());
+        dto.setCourseId(lesson.getCourse() != null ? lesson.getCourse().getCourseId() : null);
+        dto.setStudentId(lesson.getStudent() != null ? lesson.getStudent().getStudentId() : null);
+        dto.setInstructorId(lesson.getInstructor() != null ? lesson.getInstructor().getInstructorId() : null);
+        dto.setLessonDate(lesson.getLessonDate());
+        dto.setStartTime(lesson.getStartTime());
+        dto.setEndTime(lesson.getEndTime());
+        dto.setStatus(lesson.getStatus());
+        return dto;
+    }
+
+    public static Lesson toEntity(LessonsDTO dto) {
+        if (dto == null) return null;
+
+        Lesson lesson = new Lesson();
+        lesson.setLessonId(dto.getLessonId());
+        lesson.setLessonDate(dto.getLessonDate());
+        lesson.setStartTime(dto.getStartTime());
+        lesson.setEndTime(dto.getEndTime());
+        lesson.setStatus(dto.getStatus());
+        return lesson;
+    }
+
+    public static LessonsTM toTM(LessonsDTO dto){
+        if (dto == null) return null;
+
+        LessonsTM lesson = new LessonsTM();
+        lesson.setLessonId(dto.getLessonId());
+        lesson.setLessonDate(dto.getLessonDate());
+        lesson.setStartTime(dto.getStartTime().toString());
+        lesson.setEndTime(dto.getEndTime().toString());
+        lesson.setStatus(dto.getStatus());
+        return lesson;
+    }
+}
