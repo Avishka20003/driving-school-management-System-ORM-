549560867d05268152acec15faf502897aae2e18
Avishka <dilshandell333@gmail.com>
2025-09-25 16:48:59 +0530

    creat TM classes

---
+++ src/main/java/lk/ijse/dto/tm/CourseTM.java
@@ -0,0 +1,19 @@
+package lk.ijse.dto.tm;
+
+import javafx.scene.layout.Pane;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CourseTM {
+    private String courseId;
+    private String courseName;
+    private String duration;
+    private double fee;
+    private String description;
+    private String instructorId;
+    private Pane action;
+}
