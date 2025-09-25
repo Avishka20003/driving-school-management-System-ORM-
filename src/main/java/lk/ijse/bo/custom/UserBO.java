b535651d17de10abda4f89daec8b2f773322072d
Avishka <dilshandell333@gmail.com>
2025-09-25 18:47:38 +0530

    creat UserBO

---
+++ src/main/java/lk/ijse/bo/custom/UserBO.java
@@ -0,0 +1,29 @@
+package lk.ijse.bo.custom;
+
+
+
+import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.UserDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserBO extends SuperBO {
+    List<UserDTO> getAllUsers() throws Exception;
+
+    String getLastUserId() throws Exception;
+
+    boolean saveUsers(UserDTO t) throws Exception;
+
+    boolean updateUsers(UserDTO t) throws Exception;
+
+    boolean deleteUsers(String id) throws Exception;
+
+    List<String> getAllUserIds() throws Exception;
+
+    Optional<UserDTO> findByUserId(String id) throws Exception;
+
+    String generateNextUserId();
+
+    public UserDTO getUserByEmail(String email) ;
+}
