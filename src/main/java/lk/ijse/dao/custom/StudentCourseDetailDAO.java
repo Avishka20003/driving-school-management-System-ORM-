0918a21dec586b7359c9b47a30d94c51018f7b0e
Avishka <dilshandell333@gmail.com>
2025-09-25 17:21:27 +0530

    creat StudentCourseDetailsDAO

---
+++ src/main/java/lk/ijse/dao/custom/StudentCourseDetailDAO.java
@@ -0,0 +1,8 @@
+package lk.ijse.dao.custom;
+
+
+import lk.ijse.dao.CrudDAO;
+import lk.ijse.entity.StudentCourseDetails;
+
+public interface StudentCourseDetailDAO extends CrudDAO<StudentCourseDetails> {
+}
