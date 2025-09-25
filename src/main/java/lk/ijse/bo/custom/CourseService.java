fd9a03e732605b6a028d7deca981258db701a121
Avishka <dilshandell333@gmail.com>
2025-09-25 18:45:47 +0530

    creat CourseBO

---
+++ src/main/java/lk/ijse/bo/custom/CourseService.java
@@ -0,0 +1,28 @@
+package lk.ijse.bo.custom;
+
+
+
+import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.CourseDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CourseService extends SuperBO {
+    List<CourseDTO> getAllCourses() throws Exception;
+
+    String getLastCourseId() throws Exception;
+
+    boolean saveCourses(CourseDTO t) throws Exception;
+
+    boolean updateCourses(CourseDTO t) throws Exception;
+
+    boolean deleteCourses(String id) throws Exception;
+
+    List<String> getAllCourseIds() throws Exception;
+
+    Optional<CourseDTO> findByCourseId(String id) throws Exception;
+
+    String generateNewCourseId();
+
+}
