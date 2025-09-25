a1de15c1dbc5440fba6f2ac389b43bef13b43a2a
Avishka <dilshandell333@gmail.com>
2025-09-25 17:16:49 +0530

    creat DAOFactory

---
+++ src/main/java/lk/ijse/dao/DAOFactory.java
@@ -0,0 +1,36 @@
+package lk.ijse.dao;
+
+
+import lk.ijse.enums.DAOTypes;
+
+public class DAOFactory {
+    private static DAOFactory daoFactory;
+    private DAOFactory() {}
+
+    public static DAOFactory getInstance() {
+        return daoFactory == null ? (daoFactory = new DAOFactory()) : daoFactory;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends SuperDAO> T getDAO(DAOTypes daoTypes) {
+        return switch (daoTypes) {
+
+            case COURSE ->  (T) new CourseDAOImpl();
+
+            case INSTRUCTORS ->  (T) new InstructorDAOImpl();
+
+            case LESSONS ->  (T) new LessonsDAOImpl();
+
+            case PAYMENTS ->   (T) new PaymentsDAOImpl();
+
+            case STUDENT_COURSE_DETAILS ->  (T) new StudentCourseDetailsDAOImpl();
+
+            case STUDENTS -> (T) new StudentDAOImpl();
+
+            case USER -> (T) new UserDAOImpl();
+
+            case QUERY -> (T) new QueryDAOImpl();
+
+        };
+    }
+}
